8189ee4af703bd103d7278d428fc4754bc962a63
rocha <rocha@localhost>
2026-06-16 22:24:19 -0300

    feat(auth): add AuthorizationHelper component (B6)

---
+++ src/main/java/com/devappmobile/flowfuel/common/AuthorizationHelper.java
@@ -0,0 +1,30 @@
+package com.devappmobile.flowfuel.common;
+
+import com.devappmobile.flowfuel.exception.ForbiddenOperationException;
+import com.devappmobile.flowfuel.refuel.Refuel;
+import com.devappmobile.flowfuel.user.User;
+import com.devappmobile.flowfuel.vehicle.Vehicle;
+import com.devappmobile.flowfuel.vehicleevent.VehicleEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorizationHelper {
+
+    public void ensureOwnsVehicle(User user, Vehicle vehicle) {
+        if (!vehicle.getUser().getId().equals(user.getId())) {
+            throw new ForbiddenOperationException("Veículo não pertence ao usuário");
+        }
+    }
+
+    public void ensureOwnsRefuel(User user, Refuel refuel) {
+        if (!refuel.getVehicle().getUser().getId().equals(user.getId())) {
+            throw new ForbiddenOperationException("Abastecimento não pertence ao usuário");
+        }
+    }
+
+    public void ensureOwnsEvent(User user, VehicleEvent event) {
+        if (!event.getVehicle().getUser().getId().equals(user.getId())) {
+            throw new ForbiddenOperationException("Evento não pertence ao usuário");
+        }
+    }
+}
